3896a2573eafc7041a8d158c448ecc870b5b21c0
lcy0x1 <arthurlcy0x1@gmail.com>
2023-05-19 16:07:37 -0700

    migrade code to l2lib

--- src/main/java/dev/xkmc/l2backpack/events/BackpackSlotClickListener.java
+++ src/main/java/dev/xkmc/l2backpack/events/BackpackSlotClickListener.java
@@ -7,20 +7,21 @@ import dev.xkmc.l2backpack.content.remote.worldchest.WorldChestItem;
 import dev.xkmc.l2backpack.content.remote.worldchest.WorldChestMenuPvd;
 import dev.xkmc.l2backpack.init.L2Backpack;
 import dev.xkmc.l2backpack.init.advancement.BackpackTriggers;
-import dev.xkmc.l2library.init.events.click.SlotClickHandler;
+import dev.xkmc.l2library.init.events.click.writable.ClickedPlayerSlotResult;
+import dev.xkmc.l2library.init.events.click.writable.ContainerCallback;
+import dev.xkmc.l2library.init.events.click.writable.WritableStackClickHandler;
 import dev.xkmc.l2library.init.events.screen.base.ScreenTracker;
 import dev.xkmc.l2library.init.events.screen.source.PlayerSlot;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.Container;
 import net.minecraft.world.SimpleMenuProvider;
 import net.minecraft.world.entity.EquipmentSlot;
-import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.ChestMenu;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.network.NetworkHooks;
+import org.jetbrains.annotations.Nullable;
 
-public class BackpackSlotClickListener extends SlotClickHandler {
+public class BackpackSlotClickListener extends WritableStackClickHandler {
 
 	public static boolean canOpen(ItemStack stack) {
 		return stack.getItem() instanceof BaseBagItem ||
@@ -41,55 +42,60 @@ public class BackpackSlotClickListener extends SlotClickHandler {
 		slotClickToServer(-1, -1, -1);
 	}
 
+	@Nullable
 	@Override
-	public void handle(ServerPlayer player, int index, int slot, int wid) {
-		ItemStack stack;
-		Container container = null;
-		PlayerSlot<?> playerSlot;
-		AbstractContainerMenu menu = player.containerMenu;
+	protected ClickedPlayerSlotResult getSlot(ServerPlayer player, int index, int slot, int wid) {
 		if (wid == -1) {
-			stack = player.getItemBySlot(EquipmentSlot.CHEST);
-			playerSlot = PlayerSlot.ofInventory(36 + EquipmentSlot.CHEST.getIndex());
+			ItemStack stack = player.getItemBySlot(EquipmentSlot.CHEST);
+			if (canOpen(stack)) {
+				return new ClickedPlayerSlotResult(stack,
+						PlayerSlot.ofInventory(36 + EquipmentSlot.CHEST.getIndex()),
+						new PlayerInvCallback());
+			}
 			if (!canOpen(stack)) {
 				var pairOpt = CuriosCompat.getSlot(player, BackpackSlotClickListener::canOpen);
 				if (pairOpt.isPresent()) {
-					stack = pairOpt.get().getFirst();
-					playerSlot = pairOpt.get().getSecond();
+					return new ClickedPlayerSlotResult(
+							pairOpt.get().getFirst(),
+							pairOpt.get().getSecond(),
+							new PlayerInvCallback());
 				}
 			}
-		} else if (slot >= 0) {
-			stack = player.getInventory().getItem(slot);
-			playerSlot = PlayerSlot.ofInventory(slot);
-		} else {
-			if (wid == 0 || menu.containerId == 0 || wid != menu.containerId) return;
-			playerSlot = PlayerSlot.ofOtherInventory(slot, index, wid, menu);
-			stack = menu.getSlot(index).getItem();
-			container = menu.getSlot(index).container;
+			return null;
 		}
+		return super.getSlot(player, index, slot, wid);
+	}
+
+	@Override
+	protected void handle(ServerPlayer player, ClickedPlayerSlotResult result) {
 		boolean others = false;
-		if (playerSlot != null) {
-			if (wid != -1 || slot != -1 || index != -1) {
-				ScreenTracker.onServerOpen(player);
-			}
+		boolean keybind = result.container() instanceof PlayerInvCallback;
+		if (!keybind) {
+			ScreenTracker.onServerOpen(player);
 		}
-		if (playerSlot != null && stack.getItem() instanceof BaseBagItem bag) {
-			bag.open(player, playerSlot, stack);
-		} else if (stack.getItem() instanceof EnderBackpackItem) {
+		if (result.stack().getItem() instanceof BaseBagItem bag) {
+			bag.open(player, result.slot(), result.stack());
+		} else if (result.stack().getItem() instanceof EnderBackpackItem) {
 			NetworkHooks.openScreen(player, new SimpleMenuProvider((id, inv, pl) ->
-					ChestMenu.threeRows(id, inv, pl.getEnderChestInventory()), stack.getHoverName()));
-		} else if (stack.getItem() instanceof WorldChestItem chest) {
-			others = WorldChestItem.getOwner(stack).map(e -> !e.equals(player.getUUID())).orElse(false);
-			new WorldChestMenuPvd(player, stack, chest).open();
-			if (container != null) {
-				container.setChanged();
-			}
+					ChestMenu.threeRows(id, inv, pl.getEnderChestInventory()), result.stack().getHoverName()));
+		} else if (result.stack().getItem() instanceof WorldChestItem chest) {
+			others = WorldChestItem.getOwner(result.stack()).map(e -> !e.equals(player.getUUID())).orElse(false);
+			new WorldChestMenuPvd(player, result.stack(), chest).open();
+			result.container().update();
 		}
-		if (playerSlot != null) {
-			BackpackTriggers.SLOT_CLICK.trigger(player, playerSlot.type(), wid == -1 && slot == -1 && index == -1);
-		}
+		BackpackTriggers.SLOT_CLICK.trigger(player, result.slot().type(), keybind);
 		if (others) {
 			BackpackTriggers.SHARE.trigger(player);
 		}
+	}
+
+	public record PlayerInvCallback() implements ContainerCallback {
+
+		@Override
+		public void update() {
+
+		}
+
 	}
 
 }

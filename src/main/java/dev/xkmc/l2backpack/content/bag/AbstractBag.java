b5830597ad5f4d643d11eadf43fd3709c99b2971
lcy0x1 <chengyilin@umail.ucsb.edu>
2026-03-01 23:10:59 +0800

    bag texture

--- src/main/java/dev/xkmc/l2backpack/content/bag/AbstractBag.java
+++ src/main/java/dev/xkmc/l2backpack/content/bag/AbstractBag.java
@@ -19,6 +19,7 @@ import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.ClickAction;
 import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.inventory.tooltip.TooltipComponent;
 import net.minecraft.world.item.Item;
@@ -186,7 +187,7 @@ public abstract class AbstractBag extends Item
 	}
 
 	@Override
-	public ItemStack takeItem(ItemStack storage, ServerPlayer player) {
+	public ItemStack takeItem(ItemStack storage, @Nullable ServerPlayer player) {
 		var list = getContent(storage);
 		int n = getInvSize(storage);
 		for (int i = n - 1; i >= 0; i--) {
@@ -221,6 +222,22 @@ public abstract class AbstractBag extends Item
 			}
 		}
 		setContent(stack, list);
+	}
+
+	@Override
+	public boolean overrideStackedOnOther(ItemStack stack, Slot slot, ClickAction action, Player player) {
+		ItemStack slotStack = slot.getItem();
+		if (slotStack.isEmpty()) {
+			if (action == ClickAction.SECONDARY) {
+				slot.set(takeItem(stack, null));
+				return true;
+			}
+		} else if (isValidContent(slotStack)) {
+			mergeStack(stack, slotStack);
+			slot.setChanged();
+			return true;
+		}
+		return super.overrideStackedOnOther(stack, slot, action, player);
 	}
 
 	@Override

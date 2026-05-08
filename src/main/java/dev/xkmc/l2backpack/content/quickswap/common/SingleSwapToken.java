4fd74ab8b7de39f9c724a5378734f969196efb53
lcy0x1 <chengyilin@umail.ucsb.edu>
2026-05-08 21:13:10 +0800

    render container in middle

--- src/main/java/dev/xkmc/l2backpack/content/quickswap/common/SingleSwapToken.java
+++ src/main/java/dev/xkmc/l2backpack/content/quickswap/common/SingleSwapToken.java
@@ -9,8 +9,13 @@ import dev.xkmc.l2backpack.events.BackpackSel;
 import dev.xkmc.l2itemselector.init.L2ItemSelector;
 import dev.xkmc.l2itemselector.init.data.L2Keys;
 import dev.xkmc.l2itemselector.overlay.ItemWheelEntry;
+import dev.xkmc.l2itemselector.overlay.TextBox;
 import dev.xkmc.l2itemselector.overlay.WheelAdaptor;
 import dev.xkmc.l2itemselector.select.SetSelectedToServer;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Font;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import org.jetbrains.annotations.Nullable;
@@ -63,7 +68,7 @@ public record SingleSwapToken(IQuickSwapItem item, ItemStack stack, QuickSwapTyp
 		return Optional.of(new SingleWheel(this));
 	}
 
-	record SingleWheel(SingleSwapToken token) implements WheelAdaptor.ItemWheel, SwapWheel {
+	record SingleWheel(SingleSwapToken token) implements WheelAdaptor, SwapWheel {
 
 		public List<WheelAdaptor.Entry> getWheelContent() {
 			List<ItemStack> src = token.getRawList();
@@ -79,9 +84,18 @@ public record SingleSwapToken(IQuickSwapItem item, ItemStack stack, QuickSwapTyp
 			return token.getSelected();
 		}
 
-		@Override
-		public ItemStack getItem(int i) {
-			return token.getRawList().get(i);
+		public void render(GuiGraphics g, Player player) {
+			WheelAdaptor.super.render(g, player);
+			ItemStack stack = token.stack;
+			int x0 = g.guiWidth() / 2;
+			int y0 = g.guiHeight() / 2;
+			float r = (float)Math.min(x0, y0) / 2.0F;
+			float s = r * 0.03F;
+			g.pose().pushPose();
+			g.pose().translate((float)x0, (float)y0, 0.0F);
+			g.pose().scale(s, s, s);
+			g.renderItem(stack, -8, -8);
+			g.pose().popPose();
 		}
 
 		@Override

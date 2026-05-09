c04b3d19337ef7707af1625fb4be80812a0b54d8
lcy0x1 <chengyilin@umail.ucsb.edu>
2026-05-09 19:56:08 +0800

    weial suggestions

--- src/main/java/dev/xkmc/l2backpack/content/quickswap/common/SingleSwapToken.java
+++ src/main/java/dev/xkmc/l2backpack/content/quickswap/common/SingleSwapToken.java
@@ -7,8 +7,12 @@ import dev.xkmc.l2backpack.content.quickswap.type.ISingleSwapAction;
 import dev.xkmc.l2backpack.content.quickswap.type.QuickSwapType;
 import dev.xkmc.l2backpack.init.L2Backpack;
 import dev.xkmc.l2itemselector.overlay.ItemWheelEntry;
+import dev.xkmc.l2itemselector.overlay.TextBox;
 import dev.xkmc.l2itemselector.overlay.WheelAdaptor;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import org.jetbrains.annotations.Nullable;
@@ -79,7 +83,13 @@ public record SingleSwapToken(IQuickSwapItem item, ItemStack stack, QuickSwapTyp
 
 		public void render(GuiGraphics g, Player player) {
 			WheelAdaptor.super.render(g, player);
-			ItemStack stack = token.stack;
+			int index = this.getMouseSelect(player);
+			ItemStack stack = ItemStack.EMPTY;
+			if (index >= 0) {
+				stack = token.getRawList().get(index);
+			}
+			boolean tooltip = !stack.isEmpty();
+			if (stack.isEmpty()) stack = token.stack;
 			int x0 = g.guiWidth() / 2;
 			int y0 = g.guiHeight() / 2;
 			float r = (float) Math.min(x0, y0) / 2.0F;
@@ -87,13 +97,20 @@ public record SingleSwapToken(IQuickSwapItem item, ItemStack stack, QuickSwapTyp
 			g.pose().pushPose();
 			g.pose().translate((float) x0, (float) y0, 0.0F);
 			g.pose().scale(s, s, s);
-			g.renderItem(stack, -8, -8);
+			g.renderItem(stack, -8, tooltip ? -16 : -8);
 			if (!next.isEmpty()) {
-				g.pose().translate(12, 12, 0);
+				g.pose().translate(12, -4, 0);
 				g.pose().scale(0.5f, 0.5f, 0.5f);
 				g.renderItem(next, -8, -8);
 			}
 			g.pose().popPose();
+			if (tooltip) {
+				Component text = stack.getHoverName();
+				Font font = Minecraft.getInstance().font;
+				g.renderTooltip(font, stack.getHoverName(), 0, 0);
+				TextBox box = new TextBox(g, 1, 0, x0, (int) ((float) y0 + s * 3.0F), (int) r);
+				box.renderLongText(font, List.of(text));
+			}
 
 		}
 
